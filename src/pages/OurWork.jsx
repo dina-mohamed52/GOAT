43e470c257603f56203231b00bc24c9ac40c7c8d
Dina Mohamed <66019870+dina-mohamed52@users.noreply.github.com>
2024-06-22 22:11:06 +0300

    Update OurWork.jsx

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -3,10 +3,10 @@ import s from "./OurWork.module.css"
 function OurWork({id}) {
     return (
       <div  id={id} className={`${s.workcontainer}`}>
-        <div className="flex justify-center mt-[-4rem]  ">
+        <div className=" sm:mt-[-4rem] mt-[-15rem] sm:mb-0 mb-[2rem] flex justify-center   ">
           <h1
             style={{ transform: "skewX(-18deg)" }}
-            className={` ${s.sha} text-5xl  rounded-lg p-6 w-[24rem] md:text-5xl font-bold mb-8 text-center bg-gradient-to-r from-yellow-400 via-[#53b470d0] to-yellow-800 transform   text-transparent bg-clip-text shadow-xl`}
+            className={` ${s.sha} text-3xl  sm:text-5xl rounded-lg p-6 w-[24rem] md:text-5xl font-bold mb-8 text-center bg-gradient-to-r from-yellow-400 via-[#53b470d0] to-yellow-800 transform   text-transparent bg-clip-text shadow-xl`}
           >
             our campaigns
           </h1>
